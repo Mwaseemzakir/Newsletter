864e938649e19c40a47c771039d8c5bba1f57e3d
Muhammad Waseem <mwaseemzakir@outlook.com>
2023-12-02 18:54:49 +0500

    Episode 40 - Nuget Package Demo

---
+++ Episode 40 - CreateAndPublishNugetPackage/NugetPackageDemo/NugetPackageDemo/RandomGenerator.cs
@@ -0,0 +1,21 @@
+﻿public static class RandomGenerator
+{
+	private static Random random = new Random();
+	public static string RandomString(int length)
+	{
+		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		return new string(Enumerable.Repeat(chars, length)
+			.Select(s => s[random.Next(s.Length)]).ToArray());
+	}
+
+	public static int RandomInteger(int min, int max)
+	{
+		return random.Next(min, max);
+	}
+
+	public static int RandomInteger()
+	{
+		return random.Next();
+	}
+
+}
