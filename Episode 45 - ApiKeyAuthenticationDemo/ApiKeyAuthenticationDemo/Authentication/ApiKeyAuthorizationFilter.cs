e11a5082f843de3c1deeb6c91f3eac06201cfcf6
Muhammad Waseem <mwaseemzakir@outlook.com>
2024-01-05 23:13:25 +0500

    Episode 45

---
+++ Episode 45 - ApiKeyAuthenticationDemo/ApiKeyAuthenticationDemo/Authentication/ApiKeyAuthorizationFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiKeyAuthenticationDemo.Authentication;
+
+public sealed class ApiKeyAuthorizationFilter : Attribute, IAuthorizationFilter
+{
+ 
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var request = context.HttpContext.Request;
+
+        var providedApiKey = request.Headers["ApiKey"].FirstOrDefault();
+
+        var configuration = context
+                    .HttpContext
+                    .RequestServices
+                    .GetRequiredService<IConfiguration>();
+
+        var apiKey = GetApiKey(configuration);
+
+        if (string.IsNullOrWhiteSpace(providedApiKey) || providedApiKey != apiKey)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+
+  public string GetApiKey(IConfiguration configuration)
+  {
+        //This is just a demo, don't store your API key in appsettings.json
+
+        var apiKey = configuration.GetValue<string>("ApiKey");
+
+        if (string.IsNullOrWhiteSpace(apiKey))
+        {
+            throw new Exception("ApiKey is not configured");
+        }
+
+        return apiKey;
+    }
+}
---
+++ Episode 45 - ApiKeyAuthenticationDemo/ApiKeyAuthenticationDemo/Controllers/StudentControlller.cs
@@ -0,0 +1,35 @@
+﻿using ApiKeyAuthenticationDemo.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiKeyAuthenticationDemo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StudentControlller : ControllerBase
+{
+ 
+    [ApiKeyAuthorizationFilter]
+    [HttpGet]
+    public IActionResult Get()  
+    {
+        return Ok("Hello from StudentsControlller");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        return Ok($"Hello from StudentsControlller with id {id}");
+    }
+
+    [HttpPost]
+    public IActionResult Post()
+    {
+        return Ok("Hello from StudentsControlller");
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id)
+    {
+        return Ok($"Hello from StudentsControlller with id {id}");
+    }
+}
